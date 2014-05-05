3b3dc61293425ec7c2a1503fbdfdd07d59dcab0c
Mike Hall <mike@just3ws.com>
2014-05-05 00:38:10 -0500

    Basic form with a guesstimate for the cost

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,13 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function () {
+
+  $('#user_roles').change(function() {
+    $('#plan-descriptions').children().removeClass('active')
+    var selectedPlan = $("option:selected", $(this)).text().toLowerCase();
+    $('.' + selectedPlan + '-plan-description').addClass('active');
+  });
+
+});

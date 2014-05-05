7cdbcfbbcb3699724c1e87784c28ec84a7ccd8fe
Mike Hall <mike@just3ws.com>
2014-05-05 10:38:23 -0500

    Figure out which tab is active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def nav_tab(name, path)
+    "<li class=\"#{nav_tab_active(path)}\">#{link_to name, path}</li>".html_safe
+  end
+
+  def nav_tab_active(path)
+    active_nav_tab = URI.parse(String(request.original_url)).path.split('/').reject { |token| token.empty? }.first.try(:downcase)
+    current_nav_tab = URI.parse(String(path)).path.split('/').reject { |token| token.empty? }.first.try(:downcase)
+
+    if active_nav_tab == current_nav_tab
+      'active'
+    else
+      ''
+    end
+  end
 end

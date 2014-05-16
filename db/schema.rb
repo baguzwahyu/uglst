07dfab1a7d51772df8fc62695a3ca0544458e2da
Mike Hall <mike@just3ws.com>
2014-05-16 01:36:54 -0500

    Added a sponsor type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516060625) do
+ActiveRecord::Schema.define(version: 20140516063615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,12 @@ ActiveRecord::Schema.define(version: 20140516060625) do
   create_table "search_suggestions", force: true do |t|
     t.string   "term"
     t.integer  "popularity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sponsors", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
